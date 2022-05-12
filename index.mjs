94ec7c5914754b314990d20abaefbb3b761a160b
Konrad Miekus <miekus.konrad@gmail.com>
2022-05-12 07:22:58 +0200

    run single test, exist non zero when something fails

--- index.mjs
+++ index.mjs
@@ -22,11 +22,17 @@ await hasteMap.setupCachePath(hasteMapOptions)
 // jest-haste-map keeps a cache of the file system in memory and on disk so that file related operations are fast
 // whenever something changes in the file system, it's gonna be updated in the jest-haste-map's cache as well
 const { hasteFS } = await hasteMap.build()
-const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js'])
+
+const specificTestFilesToRun = process.argv[2]
+const testFiles = hasteFS.matchFilesWithGlob([
+  specificTestFilesToRun ? `**/${specificTestFilesToRun}` : '**/*.test.js'
+])
 
 const worker = new Worker(join(root, 'worker.js'), {
     enableWorkerThreads: true,
 })
+
+let hasFailed = false
 
 await Promise.all(
   Array.from(testFiles).map(async (testFile) => {
@@ -37,9 +43,18 @@ await Promise.all(
         : chalk.red.inverse( ' FAIL ')
       console.log(`${status} ${chalk.dim(relative(root, testFile)) }`)
 
-    if (!success) console.log(`  ${errorMessage}`)
+    if (!success) {
+      hasFailed = true
+      console.log(`  ${errorMessage}`)
+    }
   }),
 )
 
 worker.end()
+
+if (hasFailed) {
+    console.log(chalk.red.bold('\n Test run failed, please fix all the failing tests.'))
+    // we don't want to return status 0. if we ran it on CI we'd like that to fail
+    process.exitCode = 1
+}
 
