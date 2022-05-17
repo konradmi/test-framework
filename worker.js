46bd05bd4b0c23fcb48f0094022eef8bb2eea937
Konrad Miekus <miekus.konrad@gmail.com>
2022-05-17 07:15:52 +0200

    run tests in vm

--- worker.js
+++ worker.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const { expect } = require('expect')
 const mock = require('jest-mock')
-
+const vm = require('vm')
+const NodeEnvironment = require('jest-environment-node').default
 
 exports.runTest = async function (testFile) {
   const code = await fs.promises.readFile(testFile, 'utf8')
@@ -22,9 +23,20 @@ exports.runTest = async function (testFile) {
     const describeFns = []
     let currentDescribeFn
 
+    // describe and it could be taken from the  jest-circus package
     const describe = (name, fn) => describeFns.push([name, fn])
     const it = (name, fn) => currentDescribeFn.push([name, fn])
-    eval(code)
+
+    const environment = new NodeEnvironment({  // if we didn't use that we would have to pass to `context` things
+                                                      // like setTimeout, Buffer etc. This will create a Node-like environment
+                                                      // for tests
+      projectConfig: {
+        testEnvironmentOptions: { describe, it, expect, mock },
+      },
+    })
+    vm.runInContext(code, environment.getVmContext()) // before we used eval here. with that we can create a separate context and declare what
+                                                      // should be visible there. It's kinda similar to iFrame where you can run a code in
+                                                      // in isolation
 
     for (const [name, fn] of describeFns) {
       currentDescribeFn = []
